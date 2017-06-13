b14352e64b6a629635455a21bc196dcf78afe248
vootaaditya <adityavootafullstack@gmail.com>
2017-06-12 22:26:39 -0500

    added new files to src folder

--- reverse.js
+++ reverse.js
@@ -1,6 +1,4 @@
 var arr = [1, 2, 3];
- for(let j=arr.length;j>0;j--)
- {
-     console.log("Reverse of given number :" +j);
- }
- 
+for (var j = arr.length; j > 0; j--) {
+    console.log("Reverse of given number :" + j);
+}
---
+++ reverse.ts
@@ -0,0 +1,6 @@
+var arr = [1, 2, 3];
+ for(let j=arr.length;j>0;j--)
+ {
+     console.log("Reverse of given number :" +j);
+ }
+ 
