ff66c4cb0114499110355f040a8edd86709d20f9
vootaaditya <adityavootafullstack@gmail.com>
2017-06-13 22:17:32 -0500

    latest version of reversing

--- reverse.ts
+++ reverse.ts
@@ -1,6 +1,14 @@
-var arr = [1, 2, 3];
- for(let j=arr.length;j>0;j--)
- {
-     console.log("Reverse of given number :" +j);
- }
- 
+function revNumber()
+   {
+    var b : number=0;
+	var a:string = (<HTMLInputElement> document.getElementById("num")).value;
+    var d = parseInt(a);
+   while(d > 0)
+    {      
+        b = b * 10;
+        b = b + (d%10);
+        d =(d / 10);
+	    var c = b.toString;
+        var out = document.write("The reversed number is" + c);
+    }
+   }
